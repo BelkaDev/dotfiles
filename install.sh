ff2b6de9b5e5f7387c17b03fd5e81783b42cccc1
Belka <b.hadjoudj@nanotera.eu>
2026-05-09 03:30:54 +0200

    update tmux & zsh

--- install.sh
+++ install.sh
@@ -42,8 +42,8 @@ fi
 #   fi
 
   # bat
-  if ! command -v bat >/dev/null 2>&1 && ! command -v batcat >/dev/null 2>&1; then
-  fi
+#   if ! command -v bat >/dev/null 2>&1 && ! command -v batcat >/dev/null 2>&1; then
+#   fi
   
   # lazygit
   if ! command -v lazygit >/dev/null 2>&1; then
